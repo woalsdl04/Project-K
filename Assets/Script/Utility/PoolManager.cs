36fe5cf242c1bdc2427fd380b36cca254836d489
JeongJaemin <woalsdl0439@gmail.com>
2023-03-17 03:07:57 +0900

    Add Sound, Dotween Asset

---
+++ Assets/Script/Utility/PoolManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : Singleton<PoolManager>
+{
+    Dictionary<string, GameObject> objectPool = new Dictionary<string, GameObject>();
+
+    
+
+
+
+
+
+
+}
